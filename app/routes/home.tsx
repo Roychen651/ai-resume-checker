73bf8e2a50bd2790c619cb2a6d217970d02e3c99
Roy Chen <roy97chen@gmail.com>
2025-08-29 18:50:38 +0300

    Homepage, Navbar and Resume Card

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,35 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type {Route} from "./+types/home";
+import Navbar from "~/components/Navbar";
+import {resumes} from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        {title: "Resume Checker"},
+        {name: "description", content: "Smart feedback for your dream job"},
+    ];
 }
 
 export default function Home() {
-  return <Welcome />;
+    return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+
+        <Navbar/>
+
+        <section className="main-section">
+            <div className="page-heading py-16">
+                <h1>Resume Score Checker - ATS </h1>
+                <h2>Real-time updates on your job applications and resume feedback - Powered By AI</h2>
+            </div>
+
+
+            {resumes.length > 0 && (
+                <div className="resumes-section">
+                    {resumes.map((resume) => (
+                        <ResumeCard key={resume.id} resume={resume}/>
+                    ))}
+                </div>
+            )}
+        </section>
+
+    </main>
 }
